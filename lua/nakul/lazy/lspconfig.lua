1200ec35b68cd0c0d4e0820f76e1308c3ce08b77
nakulthebuilder <nakulkapoor1@gmail.com>
2025-04-06 16:41:33 +0400

    added svelte lsp support

--- lua/nakul/lazy/lspconfig.lua
+++ lua/nakul/lazy/lspconfig.lua
@@ -6,6 +6,16 @@ return {
     config = function()
         local lspconfig = require("lspconfig")
 
+        lspconfig.svelte.setup({
+          on_attach = function(client, bufnr)
+            local opts = { noremap = true, silent = true }
+            vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+            vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+            vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+            vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+          end,
+        })
+
         -- C/C++ setup (existing)
         lspconfig.clangd.setup({
             on_attach = function(client, bufnr)
